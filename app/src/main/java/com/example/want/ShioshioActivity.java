7142f5036ba9f8733deac4e55dcf6dca011f6728
moric <20213044-shionohayato@hcs.ac.jp>
2022-11-10 23:57:50 +0900

    初回コミット

---
+++ app/src/main/java/com/example/want/ShioshioActivity.java
@@ -0,0 +1,33 @@
+package com.example.want;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShioshioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        ImageView registerButtonImage = findViewById(R.id.registerButtonImage);
+        TextView textLogin = findViewById(R.id.textLogin);
+
+        registerButtonImage.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplication(), MainActivity.class);
+            startActivity(intent);
+        });
+
+        textLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplication(), LoginActivity.class);
+            startActivity(intent);
+        });
+        System.out.println("shioshio");
+
+
+    }
+}
